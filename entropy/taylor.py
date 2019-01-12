9bc85225bc77d0a5288d06665719941bca9bfd84
mikesy <michaelyoung.aero@gmail.com>
2019-01-11 20:45:46 -0600

    to match adaptive auto study

--- entropy/taylor.py
+++ entropy/taylor.py
@@ -1,22 +1,22 @@
-import entropy
+from . import entropy
 
-def get_entropy(ucmd, dist, alpha):
+def get_entropy(ucmd, alpha):
     #pass alpha as zero if clean
-    THETA_ROW = 2
-    u_errors = get_command_errors_1d(ucmd, THETA_ROW)
+    # THETA_ROW = 2
+    u_errors = get_command_errors_1d(ucmd) #, THETA_ROW)
 
-    if dist == 'clean':
-        alpha = entropy.calc_alpha(u_errors[0])   #only calculate for non-distracted
+    # if dist == 'clean':
+    #     alpha = entropy.calc_alpha(u_errors[0])   #only calculate for non-distracted
 
     # stats
     entropies = entropy.calc_entropy(u_errors,alpha)
 
-    return entropies, alpha
+    return entropies #, alpha
 
-def get_command_errors_1d(ucmd,  command_row):
+def get_command_errors_1d(ucmd):
     all_seg_errors = []
     for segment_i in range(0,len(ucmd)):        # cycle through segments split by L2
-        u_seg = ucmd[segment_i][command_row]    # segments should be split by the L2 presses
+        u_seg = ucmd[segment_i]    # segments should be split by the L2 presses
         u_errors = []
         for u_i in range(0,len(u_seg)):
             u_snip = u_seg[u_i:4+u_i]
