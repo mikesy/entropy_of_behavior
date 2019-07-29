4331071cf955b467966db1be78c272e8219c5bfb
mikesy <michaelyoung.aero@gmail.com>
2019-07-29 09:45:40 -0500

    new analysis

--- demo/behavioral_entropy_ND_discrete_demo.py
+++ demo/behavioral_entropy_ND_discrete_demo.py
@@ -4,7 +4,7 @@ sys.path.append('../')
 sys.path.append('../../')
 
 import argparse
-from entropy import discrete
+from entropy.discrete import discrete_entropy_functions as discrete
 import itertools
 import matplotlib.pyplot as plt
 import numpy as np
@@ -19,10 +19,12 @@ class CompareDNDEB:
         self.classes = ["1", "2", "3"]
 
         self.create_single_plot_multiple_examples()
+        # self.create_single_plot_multiple_examples_off_diag()
 
     def create_single_plot_multiple_examples(self):
 
         fig = plt.figure(self.figure_i)
+        plt.rc('text', usetex=True)
         self.figure_i += 1
         normalize = False
         if normalize:
@@ -33,7 +35,7 @@ class CompareDNDEB:
         plt.subplot(151)
         cm = np.array([[33, 0, 0], [0, 33, 0], [0, 0, 33]])
         self.plot_confusion_matrix_for_subplot(cm,norm, show_y_ticks=True,normalize=normalize)
-        plt.ylabel('True Label')
+        plt.ylabel(r'True Command $\textbf{u}$')
         plt.xlabel('(a)')
         
         plt.subplot(152)
@@ -57,7 +59,7 @@ class CompareDNDEB:
         plt.xlabel('(e)')
 
         # ax.set_xlabel('True Label')
-        fig.text(0.525, 0.28, 'Predicted label', ha='center', va='center')
+        fig.text(0.525, 0.28, r'Predicted Command $\tilde{\textbf{u}}$', ha='center', va='center')
         if self.save_plots:
             plt.savefig('../plots/comparison_of_5_discrete_entropy_types.eps',
                         format='eps', dpi=1000, bbox_inches='tight')
@@ -67,14 +69,19 @@ class CompareDNDEB:
     def plot_confusion_matrix_for_subplot(self, cm, norm,
                               show_y_ticks=False,
                               normalize=False,
-                              cmap=plt.cm.Greens):
+                              cmap=plt.cm.Greens,
+                              use_off_diags=False):
         """
         This function prints and plots the confusion matrix.
         Normalization can be applied by setting `normalize=True`.
         """
 
-        H = discrete.calc_entropy(cm)
-        title = "$H_t$ = %0.2f" %H
+        if use_off_diags:
+            H = discrete.calc_entropy_with_off_diags(cm)
+            title = "$H_t$ = %0.2f, %0.2f" %(H[0], H[1])
+        else:
+            H = discrete.calc_entropy(cm)
+            title = "$H_t$ = %0.2f" % H
 
         if normalize:
             cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
@@ -98,6 +105,50 @@ class CompareDNDEB:
                     color="white" if cm[i, j] > thresh else "black")
 
         plt.tight_layout()
+
+    def create_single_plot_multiple_examples_off_diag(self):
+        fig = plt.figure(self.figure_i)
+        self.figure_i += 1
+        normalize = False
+        if normalize:
+            vmax = 1
+        else:
+            vmax = 99
+        norm = colors.Normalize(vmin=0, vmax=vmax)
+        plt.subplot(151)
+        cm = np.array([[33, 0, 0], [0, 33, 0], [0, 0, 33]])
+        self.plot_confusion_matrix_for_subplot(
+            cm, norm, show_y_ticks=True, normalize=normalize, use_off_diags=True)
+        plt.ylabel('True Label')
+        plt.xlabel('(a)')
+
+        plt.subplot(152)
+        cm = np.array([[31, 1, 1], [1, 31, 1], [1, 1, 31]])
+        self.plot_confusion_matrix_for_subplot(cm, norm, normalize=normalize, use_off_diags=True)
+        plt.xlabel('(b)')
+
+        plt.subplot(153)
+        cm = np.array([[11, 11, 11], [11, 11, 11], [11, 11, 11]])
+        self.plot_confusion_matrix_for_subplot(cm, norm, normalize=normalize, use_off_diags=True)
+        plt.xlabel('(c)')
+
+        plt.subplot(154)
+        cm = np.array([[3, 15, 15], [15, 3, 15], [15, 15, 3]])
+        self.plot_confusion_matrix_for_subplot(cm, norm, normalize=normalize, use_off_diags=True)
+        plt.xlabel('(d)')
+
+        plt.subplot(155)
+        cm = np.array([[80, 0, 0], [1, 8, 1], [2, 2, 5]])
+        self.plot_confusion_matrix_for_subplot(cm, norm, normalize=normalize, use_off_diags=True)
+        plt.xlabel('(e)')
+
+        # ax.set_xlabel('True Label')
+        fig.text(0.525, 0.28, 'Predicted label', ha='center', va='center')
+        if self.save_plots:
+            plt.savefig('../plots/comparison_of_5_discrete_entropy_types.eps',
+                        format='eps', dpi=1000, bbox_inches='tight')
+            plt.savefig('../plots/comparison_of_5_discrete_entropy_types.png',
+                        format='png', dpi=1000, bbox_inches='tight')
 
 if __name__=='__main__':
 
--- entropy/discrete/discrete_entropy_functions.py
+++ entropy/discrete/discrete_entropy_functions.py
@@ -61,3 +61,28 @@ def calc_entropy(cm):
     if math.isnan(entropy):
         print("got a nan")
     return entropy
+
+def calc_entropy_with_off_diags(cm):
+    num_classes = np.shape(cm)[0]
+    num_bins = num_classes**2
+    num_predictions_off_diag = np.sum(cm.flatten()) - np.trace(cm)
+    num_predictions_diag = np.trace(cm)
+    entropy_off_diag = 0
+    entropy_diag = 0
+    for row_i, row in enumerate(cm):
+        for col_i, b in enumerate(row):
+            if row_i == col_i:
+                num_predictions = num_predictions_diag
+            else:
+                num_predictions = num_predictions_off_diag
+            p_b = float(b)/num_predictions      # probability of bin b
+            if p_b < 1e-10:
+                p_b = 1e-10
+            if row_i == col_i:
+                entropy_diag += -p_b*np.log(p_b)/np.log(num_classes)
+            else:
+                entropy_off_diag += -p_b*np.log(p_b)/np.log(num_bins - num_classes)
+            # entropy += -p_b*np.log(p_b)/np.log(num_bins)
+    if math.isnan(entropy_diag) or math.isnan(entropy_off_diag):
+        print("got a nan")
+    return [entropy_diag, entropy_off_diag] 
